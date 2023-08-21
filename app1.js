136de9e52851c5d9bde5217289dd18a25bdc014b
sheerin-farhana <sheerinfarhana25@gmail.com>
2023-08-21 10:35:53 +0530

    add-product project done

---
+++ app1.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    // console.log('In another Middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
+});
+
+app.post('/product',(req,res,next) => {
+    console.log(req.body.title);
+    console.log(req.body.size);
+    res.redirect('/');
+
+});
+
+app.use('/',(req,res,next)=>{
+    // console.log('In another Middleware');
+    //sending a response
+    res.send('<h1>Hello from Express!</h1>')
+});
+
+app.listen(3000);
