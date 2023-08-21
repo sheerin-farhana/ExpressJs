41c15aa8d39fc1a70f7a213c13434f4bc7157f72
sheerin-farhana <sheerinfarhana25@gmail.com>
2023-08-21 11:11:19 +0530

    routers and filters used to clean code

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the Middleware');
-    next(); // allows the request to continue to the next middleware in line
-})
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
 
-app.use((req,res,next)=>{
-    console.log('In another Middleware');
-    //sending a response
-    res.send('<h1>Hello from Express!</h1>')
-})
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+
+
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page Not Found !!</h1>');
+});
+
 app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/add-product',(req,res,next)=>{
+    // console.log('In another Middleware');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+});
+
+router.post('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+
+});
+
+
+module.exports = router;
